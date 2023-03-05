4420e46e708617cf2069f8cfd8fc581cb47cad08
Aaron Rasing <maaronrasing@gmail.com>
2023-03-05 14:54:02 +0800

    Spring Cloud config server

--- bushub-config/src/main/java/com/bushub/config/BushubConfigApplication.java
+++ bushub-config/src/main/java/com/bushub/config/BushubConfigApplication.java
@@ -1,13 +1,16 @@
 package com.bushub.config;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BushubConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BushubConfigApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BushubConfigApplication.class, args);
+  }
 
 }
