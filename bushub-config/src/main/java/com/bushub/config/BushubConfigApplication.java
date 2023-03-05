69a2d782aaddf6535bd46e286518f033373a328c
Aaron Rasing <maaronrasing@gmail.com>
2023-03-05 14:21:39 +0800

    Added config server + changed ports for uniformity

---
+++ bushub-config/src/main/java/com/bushub/config/BushubConfigApplication.java
@@ -0,0 +1,13 @@
+package com.bushub.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BushubConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BushubConfigApplication.class, args);
+	}
+
+}
---
+++ bushub-config/src/test/java/com/bushub/config/BushubConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bushub.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BushubConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
