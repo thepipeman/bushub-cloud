ca18725bb7288ba64eb6bbe6c10dc7a15f0d3100
Aaron Rasing <maaronrasing@gmail.com>
2023-03-27 07:05:48 +0800

    Added gateway

---
+++ bushub-gateway/src/main/java/com/bushub/gateway/BushubGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bushub.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BushubGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BushubGatewayApplication.class, args);
+  }
+
+}
---
+++ bushub-gateway/src/test/java/com/bushub/gateway/BushubGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bushub.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BushubGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
