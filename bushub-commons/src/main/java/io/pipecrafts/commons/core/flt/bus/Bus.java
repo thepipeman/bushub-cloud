35ad3861c00c8c7075dc65050fa4ea5890ab920d
Aaron Rasing <maaronrasing@gmail.com>
2025-04-04 23:20:52 +0800

    Sample docs

--- bushub-commons/src/main/java/io/pipecrafts/commons/core/flt/bus/Bus.java
+++ bushub-commons/src/main/java/io/pipecrafts/commons/core/flt/bus/Bus.java
@@ -1,18 +1,41 @@
 package io.pipecrafts.commons.core.flt.bus;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record Bus(
 
-
+  @Schema(
+    description = "Primary ID of the bus",
+    example = "1L",
+    accessMode = Schema.AccessMode.READ_ONLY
+  )
   Long id,
+
+  @Schema(
+    description = "Type of the bus",
+    example = "FIRST_CLASS",
+    requiredMode = Schema.RequiredMode.AUTO,
+    accessMode = Schema.AccessMode.READ_WRITE
+  )
   @NotNull
   BusType type,
 
+  @Schema(
+    description = "Plate number of the bus",
+    example = "ABC12345",
+    requiredMode = Schema.RequiredMode.REQUIRED,
+    accessMode = Schema.AccessMode.READ_WRITE
+  )
   @NotBlank
   String plateNumber,
 
+  @Schema(
+    description = "Unique number of the bus. Null values will result to automatically created values based on format configured",
+    example = "BAA1234",
+    accessMode = Schema.AccessMode.READ_WRITE
+  )
   String number
 ) {
 }
