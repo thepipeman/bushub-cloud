01ace2852caccb793fd0ecb68b6da0b19a91d186
Aaron Rasing <maaronrasing@gmail.com>
2025-04-12 15:45:20 +0800

    Ignored integration tests

--- bushub-core/src/test/java/io/pipecrafts/core/io/pipecrafts/core/fleet/bus/BusServiceApplicationTests.java
+++ bushub-core/src/test/java/io/pipecrafts/core/io/pipecrafts/core/fleet/bus/BusServiceApplicationTests.java
@@ -2,7 +2,7 @@ package io.pipecrafts.core.io.pipecrafts.core.fleet.bus;
 
 import io.pipecrafts.commons.core.flt.bus.Bus;
 import io.pipecrafts.commons.core.flt.bus.BusType;
-import org.junit.jupiter.api.Test;
+import org.junit.Ignore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -10,6 +10,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+@Ignore
 @SpringBootTest(
   webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
@@ -18,7 +19,7 @@ public class BusServiceApplicationTests {
   @Autowired
   private WebTestClient webTestClient;
 
-  @Test
+//  @Test
   public void whenPostRequestThenBookCreated() {
     final var bus = new Bus(1L, BusType.FIRST_CLASS, "ABC123458", "123453");
 
