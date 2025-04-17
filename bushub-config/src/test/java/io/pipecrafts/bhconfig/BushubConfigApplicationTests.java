3032d1795b43dfca8514a3e1851eeb6dfedae113
Aaron Rasing <maaronrasing@gmail.com>
2025-04-17 23:25:08 +0800

    Added externalized configuration and setup for core as config-client

---
+++ bushub-config/src/test/java/io/pipecrafts/bhconfig/BushubConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pipecrafts.bhconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BushubConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
