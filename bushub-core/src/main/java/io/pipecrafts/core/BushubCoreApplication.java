532cf87c1020532826d9b489a39771b264a48aba
Aaron Rasing <maaronrasing@gmail.com>
2025-04-17 22:22:02 +0800

    Added annotation processor for properties

--- bushub-core/src/main/java/io/pipecrafts/core/BushubCoreApplication.java
+++ bushub-core/src/main/java/io/pipecrafts/core/BushubCoreApplication.java
@@ -2,10 +2,12 @@ package io.pipecrafts.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("io.pipecrafts")
+@ConfigurationPropertiesScan
 public class BushubCoreApplication {
 
 	public static void main(String[] args) {
